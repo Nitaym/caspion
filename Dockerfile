d3b412799d69488c5326870240ad574c4b775cee
Nitay Megides <nitay@edgybees.com>
2022-08-09 13:12:20 +0300

    WIP

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM andrewmackrodt/nodejs-chromium
-COPY . /app
-WORKDIR /app
-RUN npm i
-RUN yarn serve
-ENTRYPOINT yarn serve
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-docker run -it --entrypoint=/bin/bash -v $(pwd):/code caspion-dev
+docker build -t caspion-infra -f Dockerfile-Infra .
+# docker build -t caspion-local-dev -f Dockerfile-Dev .
+
+# docker run -it --entrypoint=/bin/bash -v $(pwd):/code caspion-local
+# docker run -it --entrypoint=/bin/bash -v $(pwd):/code caspion-dev
 # docker run -it --entrypoint=/bin/bash -v $(pwd):/code andrewmackrodt/nodejs-chromium
